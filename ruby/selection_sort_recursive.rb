af17219b45a938824a9fe26ab7f42a05a00518fb
Michael Lobman <michael.lobman@gmail.com>
2022-03-27 11:12:51 -0400

    Complete assignment

--- ruby/selection_sort_recursive.rb
+++ ruby/selection_sort_recursive.rb
@@ -1,6 +1,19 @@
+
 def selection_sort_recursive(arr)
-  # type your code in here
+  return arr if arr.empty?
+  
+  min = arr.min
+  idx = arr.index(min)
+  arr.delete_at(idx)
+
+  result = selection_sort_recursive(arr)
+  result.unshift(min)
+
 end
+
+test_arr = [5, 4, 3, 2, 1]
+
+print selection_sort_recursive(test_arr)
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: [-1, 2, 3, 5]"
@@ -12,5 +25,10 @@ if __FILE__ == $PROGRAM_NAME
   # Don't forget to add your own!
 end
 
+# Given an array, recursively sort it
+# from the least to the greatest
+
 # Please add your pseudocode to this file
+# If array length is one, return the array
+# 
 # And a written explanation of your solution
